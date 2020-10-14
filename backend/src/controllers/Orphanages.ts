1d4c99832c923d43794d6d6bb2fd239c3fda622b
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2020-10-14 19:57:38 -0300

    day 3 completed

--- backend/src/controllers/Orphanages.ts
+++ backend/src/controllers/Orphanages.ts
@@ -1,18 +1,25 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
+import orphanageView from '../views/orphanages';
+import * as Yup from 'yup';
+
 import Orphanage from '../models/Orphanage';
 
 export default {
   async index(req:Request, res:Response) {
     const orphanagesRepository = getRepository(Orphanage);
-    const orphanages = await orphanagesRepository.find();
-    return res.json(orphanages);
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images']
+    });
+    return res.json(orphanageView.renderMany(orphanages));
   },
   async show(req:Request, res:Response) {
     const { id } = req.params;
     const orphanagesRepository = getRepository(Orphanage);
-    const orphanage = await orphanagesRepository.findOneOrFail(id);
-    return res.json(orphanage);
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {
+      relations: ['images']
+    });
+    return res.json(orphanageView.render(orphanage));
   },
   async create(req:Request, res:Response) {
     const {
@@ -32,7 +39,7 @@ export default {
       return { path: image.filename }
     });
 
-    const orphanage = orphanagesRepository.create({
+    const data = {
       name,
       latitude,
       longitude,
@@ -41,7 +48,28 @@ export default {
       opening_hours,
       open_on_weekends,
       images
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      )
     });
+
+    await schema.validate(data, {
+      abortEarly: false
+    });
+
+    const orphanage = orphanagesRepository.create(data);
   
     await orphanagesRepository.save(orphanage);
   
